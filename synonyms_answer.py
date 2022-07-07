d7378645f70dbdfce25ce67e9b474084bb52e67f
bsneider <54788551+bsneider@users.noreply.github.com>
2022-07-07 09:42:29 -0400

    commit answer

---
+++ synonyms_answer.py
@@ -0,0 +1,96 @@
+import json
+
+
+class DisjointSet(object):
+    def __init__(self):
+        self.parents = {}
+
+    def get_root(self, w):
+        words_traversed = []
+        while w in self.parents and self.parents[w] != w:
+            words_traversed.append(w)
+            w = self.parents[w]
+        for word in words_traversed:
+            self.parents[word] = w
+        return w
+
+    def add_synonyms(self, w1, w2):
+        if w1 not in self.parents:
+            self.parents[w1] = w1
+        if w2 not in self.parents:
+            self.parents[w2] = w2
+
+        w1_root = self.get_root(w1)
+        w2_root = self.get_root(w2)
+        if w1_root < w2_root:
+            w1_root, w2_root = w2_root, w1_root
+        self.parents[w2_root] = w1_root
+
+    def are_synonymous(self, w1, w2):
+        r1 = self.get_root(w1)
+        r2 = self.get_root(w2)
+        print(f"r1 {r1}, r2 {r2}")
+        return r1 == r2
+
+
+def preprocess_synonyms(synonym_words):
+    ds = DisjointSet()
+    for w1, w2 in synonym_words:
+        ds.add_synonyms(w1.lower(), w2.lower())
+    return ds
+
+
+def synonym_queries(synonym_words, queries):
+    """
+    will ensure that all synonyms and queries are put to lower case so comparisons work
+    pre-processing words into a disjoint set will allow fastest look up and can confirm if they are synonyms if they have the same root
+    """
+    synonyms = preprocess_synonyms(synonym_words)
+
+    output = []
+    for q1, q2 in queries:
+        q1, q2 = q1.split(), q2.split()
+        if len(q1) != len(q2):
+            output.append("different")
+            continue
+        result = "synonyms"
+        for i in range(len(q1)):
+            w1, w2 = q1[i].lower(), q2[i].lower()
+            if w1 == w2:
+                continue
+            elif synonyms.are_synonymous(w1, w2):
+                continue
+            result = "different"
+            break
+        output.append(result)
+    return output
+
+
+if __name__ == "__main__":
+    ## TODO change to take input from argvars for file names
+    f = open("./inputs/example.in.json")
+    expected_output = open("./outputs/example.out")
+    data = json.load(f)
+    print(json.dumps(data))
+    with open("./outputs/example.txt", "w") as w:
+        ## Take the input and make the output
+        for testCase in data.get("testCases"):
+            result = synonym_queries(
+                testCase.get("dictionary"), testCase.get("queries")
+            )
+            w.write("\n".join(result))
+            w.write("\n")
+            print(result)
+        f = open("./inputs/example_big.in.json")
+    expected_output = open("./outputs/example_big.out")
+    data = json.load(f)
+    print(json.dumps(data))
+    with open("./outputs/example_big.txt", "w") as w:
+        ## Take the input and make the output
+        for testCase in data.get("testCases"):
+            result = synonym_queries(
+                testCase.get("dictionary"), testCase.get("queries")
+            )
+            w.write("\n".join(result))
+            w.write("\n")
+            print(result)
--- tests/test_synonyms.py
+++ tests/test_synonyms.py
@@ -1,8 +1,12 @@
 import json
+import os
 
 # import transitivity_disjoint_set as soln
 
 def test_readinput():
     #read file
-    
-    f = open('./inputs/')
+    cwd = os.getcwd()
+
+    # Print the current working directory
+    print("Current working directory: {0}".format(cwd))
+    f = open('../inputs/example.in')
